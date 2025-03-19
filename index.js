71c102c365f862e69f248891380e501c19644e40
Kushal Vardhan <vardhank140102@gmail.com>
2025-03-19 13:57:12 +0530

    mood history functionality

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-const date = new Date()
-const currYear = date.getFullYear()
+const showMoodHistory = document.querySelector("#showMoodHistory")
+const calenderWrapper = document.querySelector(".calenderWrapper")
+calenderWrapper.style.display = "none"
+
+showMoodHistory.addEventListener('click', () => {
+
+    if (calenderWrapper.style.display === "none") {
+
+        calenderWrapper.style.display = "block"
+        showMoodHistory.innerText = "Hide Mood History"
+        calenderRender()
+    }
+    else {
+        showMoodHistory.innerText = "Show Mood History"
+
+        calenderWrapper.style.display = "none"
+    }
+})
+
+
+const moodEmojis = document.querySelectorAll(".emojis")
+const ques = document.querySelector("#ques")
+const todaysMood = document.querySelector(".todaysMood")
+moodEmojis.forEach((emoji) => {
+    emoji.addEventListener("click", () => {
+        ques.remove()
+        const emojisText = emoji.alt
+        todaysMood.innerHTML=""
+        const mood = document.createElement("h3")
+        const moodEmoji = document.createElement("img")
+        mood.innerText = `Today's Mood :${emojisText}`
+        moodEmoji.src = emoji.src
+        moodEmoji.classList.add("emojiInHeading")
+        mood.append(moodEmoji)
+        todaysMood.append(mood)
+    })
+})
+
+
+
+let date = new Date()
+let currYear = date.getFullYear()
 const currentDate = document.querySelector(".currentDate")
 let currMonth = date.getMonth()
 function calenderRender() {
@@ -16,36 +56,44 @@ function calenderRender() {
         "Oct",
         "Nov",
         "Dec",
-      ];
+    ];
     currentDate.innerText = `${monthNames[currMonth]} ${currYear} `
     const days = document.querySelector(".days")
     const firstDayOfMonth = new Date(currYear, currMonth, 1).getDay()
     const lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate()
     const lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate()
-    const lastDayOfMonth = new Date(currYear,currMonth,lastDateOfMonth).getDay()
+    const lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay()
     console.log(lastDayOfMonth);
-    
+
     let dateListTag = ""
-    for(let i = firstDayOfMonth;i>0;i--){
-        dateListTag += `<li>${lastDateOfLastMonth - i+1}</li>`
+    for (let i = firstDayOfMonth; i > 0; i--) {
+        dateListTag += `<li>${lastDateOfLastMonth - i + 1}</li>`
     }
     for (let i = 1; i <= lastDateOfMonth; i++) {
         dateListTag += `<li>${i}</li>`
     }
 
-    for(let i = lastDayOfMonth;i<6;i++){
-        dateListTag += `<li>${i - lastDayOfMonth+1}</li>`
+    for (let i = lastDayOfMonth; i < 6; i++) {
+        dateListTag += `<li>${i - lastDayOfMonth + 1}</li>`
 
     }
     days.innerHTML = dateListTag
 }
 
-calenderRender()
 
 const prevNext = document.querySelectorAll(".icons span")
-prevNext.forEach((icon)=>{
-    icon.addEventListener('click',()=>{
-     currMonth= icon.id ==="prev" ? currMonth - 1 : currMonth + 1
+prevNext.forEach((icon) => {
+    icon.addEventListener('click', () => {
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1
+
+        if (currMonth < 0 || currMonth > 11) {
+            date = new Date(currYear, currMonth, new Date().getDate())
+            currYear = date.getFullYear()
+            currMonth = date.getMonth()
+        } else {
+            date = new Date(currYear, currMonth, 1)
+        }
+
         calenderRender()
     })
 })
